9b7ac4e3e58301aa0046ccd6791012a83059eca7
mehran-khanjan <mehrankh1913@gmail.com>
2023-05-13 09:00:18 +0330

    first commit - addd interface

---
+++ DEXRouter.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+pragma solidity >=0.8.2;
+
+interface ISushiswapV2Pair {
+    event Approval(address indexed owner, address indexed spender, uint value);
+    event Transfer(address indexed from, address indexed to, uint value);
+}

29d49080a5e495b044a7c8653aaaf251349175c6
mehran-khanjan <mehrankh1913@gmail.com>
2023-05-18 10:12:14 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -13,5 +13,9 @@ interface ISushiswapV2Pair {
     function balanceOf(address owner) external view returns (uint);
     function allowance(address owner, address spender) external view returns (uint);
 
+    function approve(address spender, uint value) external returns (bool);
+    function transfer(address to, uint value) external returns (bool);
+    function transferFrom(address from, address to, uint value) external returns (bool);
+
     
 }

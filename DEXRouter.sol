beb6b00d8eb00af90484f065e1696f65209cef84
mehran-khanjan <mehrankh1913@gmail.com>
2023-05-22 09:30:55 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -21,5 +21,7 @@ interface ISushiswapV2Pair {
     function PERMIT_TYPEHASH() external pure returns (bytes32);
     function nonces(address owner) external view returns (uint);
 
+    function permit(address owner, address spender, uint value, uint deadline, uint8 v, bytes32 r, bytes32 s) external;
+
     
 }

7a5f1ff9837ea59e117efa23abfdb41950c43e2d
mehran-khanjan <mehrankh1913@gmail.com>
2023-05-29 10:39:42 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -39,5 +39,6 @@ interface ISushiswapV2Pair {
     function factory() external view returns (address);
     function token0() external view returns (address);
     function token1() external view returns (address);
+    function getReserves() external view returns (uint112 reserve0, uint112 reserve1, uint32 blockTimestampLast);
     
 }

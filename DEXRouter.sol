762f6094ced9d3b34d6ee9d31f7d13004cfad0bd
mehran-khanjan <mehrankh1913@gmail.com>
2023-06-14 09:04:30 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -49,5 +49,6 @@ interface ISushiswapV2Pair {
     function swap(uint amount0Out, uint amount1Out, address to, bytes calldata data) external;
     function skim(address to) external;
     function sync() external;
+    function initialize(address, address) external;
     
 }

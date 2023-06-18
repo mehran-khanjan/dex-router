fb94aff19865db31fd0e2a52b3e5a9a7442daa17
mehran-khanjan <mehrankh1913@gmail.com>
2023-06-18 09:02:10 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -60,4 +60,8 @@ library SafeMathSushiswap {
     function sub(uint x, uint y) internal pure returns (uint z) {
         require((z = x - y) <= x, 'ds-math-sub-underflow');
     }
+
+    function mul(uint x, uint y) internal pure returns (uint z) {
+        require(y == 0 || (z = x * y) / y == x, 'ds-math-mul-overflow');
+    }
 }

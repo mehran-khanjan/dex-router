c71827c99a06f5c7639abc6aca4fc030288174d1
mehran-khanjan <mehrankh1913@gmail.com>
2023-06-20 09:15:31 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -75,4 +75,15 @@ library SushiswapV2Library {
         (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'SushiswapV2Library: ZERO_ADDRESS');
     }
+
+    // calculates the CREATE2 address for a pair without making any external calls
+    function pairFor(address factory, address tokenA, address tokenB) internal pure returns (address pair) {
+        (address token0, address token1) = sortTokens(tokenA, tokenB);
+        pair = address(uint160(uint256(keccak256(abi.encodePacked(
+                hex'ff',
+                factory,
+                keccak256(abi.encodePacked(token0, token1)),
+                hex'e18a34eb0e04b04f7a0ac29a6e80748dca96319b42c54d679cb821dca90c6303' // init code hash
+            )))));
+    }
 }

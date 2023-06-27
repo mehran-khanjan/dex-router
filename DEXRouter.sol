7533e7cfe7ad353ca79973ab3ea8bd7e3bfde87b
mehran-khanjan <mehrankh1913@gmail.com>
2023-06-27 11:22:59 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -130,4 +130,15 @@ library SushiswapV2Library {
             amounts[i + 1] = getAmountOut(amounts[i], reserveIn, reserveOut);
         }
     }
+
+    // performs chained getAmountIn calculations on any number of pairs
+    function getAmountsIn(address factory, uint amountOut, address[] memory path) internal view returns (uint[] memory amounts) {
+        require(path.length >= 2, 'SushiswapV2Library: INVALID_PATH');
+        amounts = new uint[](path.length);
+        amounts[amounts.length - 1] = amountOut;
+        for (uint i = path.length - 1; i > 0; i--) {
+            (uint reserveIn, uint reserveOut) = getReserves(factory, path[i - 1], path[i]);
+            amounts[i - 1] = getAmountIn(amounts[i], reserveIn, reserveOut);
+        }
+    }
 }

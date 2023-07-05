725b6b4f2bbc472593f3966949152eb20305237d
mehran-khanjan <mehrankh1913@gmail.com>
2023-07-05 08:46:38 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -162,4 +162,9 @@ library TransferHelper {
         (bool success, bytes memory data) = token.call(abi.encodeWithSelector(0x23b872dd, from, to, value));
         require(success && (data.length == 0 || abi.decode(data, (bool))), 'TransferHelper: TRANSFER_FROM_FAILED');
     }
+
+    function safeTransferNative(address to, uint value) internal {
+        (bool success,) = to.call{value:value}(new bytes(0));
+        require(success, 'TransferHelper: NATIVE_TRANSFER_FAILED');
+    }
 }

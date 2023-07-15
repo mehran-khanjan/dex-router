1f9aacdfbd50e071f5a570028a0de44fe9f3a412
mehran-khanjan <mehrankh1913@gmail.com>
2023-07-15 08:49:57 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -179,4 +179,7 @@ interface ISushiswapV2Factory {
     function getPair(address tokenA, address tokenB) external view returns (address pair);
     function allPairs(uint) external view returns (address pair);
     function allPairsLength() external view returns (uint);
+
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+
 }

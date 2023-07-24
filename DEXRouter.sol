e7b566612bb191fdbfd84726795967423fb9e11a
mehran-khanjan <mehrankh1913@gmail.com>
2023-07-24 09:21:34 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -196,4 +196,5 @@ interface IwNATIVE {
 interface AnyswapV1ERC20 {
     function mint(address to, uint256 amount) external returns (bool);
     function burn(address from, uint256 amount) external returns (bool);
+    function changeVault(address newVault) external returns (bool);
 }

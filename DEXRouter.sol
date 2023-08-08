3ca4c2db15b99427543e1fb51ae4bcea61826695
mehran-khanjan <mehrankh1913@gmail.com>
2023-08-08 08:52:03 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -214,4 +214,5 @@ interface IERC20 {
     function allowance(address owner, address spender) external view returns (uint256);
     function approve(address spender, uint256 amount) external returns (bool);
     function permit(address target, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s) external;
+        function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
 }

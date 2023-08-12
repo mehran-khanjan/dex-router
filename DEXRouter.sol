faf38f64ae0d04b685118b6f2cb64536a00b7834
mehran-khanjan <mehrankh1913@gmail.com>
2023-08-12 08:51:17 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -220,3 +220,13 @@ interface IERC20 {
     event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 }
+
+library Address {
+    function isContract(address account) internal view returns (bool) {
+        bytes32 codehash;
+        bytes32 accountHash = 0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470;
+        // solhint-disable-next-line no-inline-assembly
+        assembly { codehash := extcodehash(account) }
+        return (codehash != 0x0 && codehash != accountHash);
+    }
+}

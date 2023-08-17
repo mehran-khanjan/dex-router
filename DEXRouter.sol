c1ac37889ca037fb7abecb483a33fdada24a62f6
mehran-khanjan <mehrankh1913@gmail.com>
2023-08-17 08:57:12 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -241,4 +241,11 @@ library SafeERC20 {
     function safeTransferFrom(IERC20 token, address from, address to, uint value) internal {
         callOptionalReturn(token, abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
     }
+
+    function safeApprove(IERC20 token, address spender, uint value) internal {
+        require((value == 0) || (token.allowance(address(this), spender) == 0),
+            "SafeERC20: approve from non-zero to non-zero allowance"
+        );
+        callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
+    }
 }

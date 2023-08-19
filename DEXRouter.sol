a5005e140ef27b7e8a7fa9b989df188254c3e81d
mehran-khanjan <mehrankh1913@gmail.com>
2023-08-19 08:51:56 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -248,4 +248,17 @@ library SafeERC20 {
         );
         callOptionalReturn(token, abi.encodeWithSelector(token.approve.selector, spender, value));
     }
+
+    function callOptionalReturn(IERC20 token, bytes memory data) private {
+        require(address(token).isContract(), "SafeERC20: call to non-contract");
+
+        // solhint-disable-next-line avoid-low-level-calls
+        (bool success, bytes memory returndata) = address(token).call(data);
+        require(success, "SafeERC20: low-level call failed");
+
+        if (returndata.length > 0) { // Return data is optional
+            // solhint-disable-next-line max-line-length
+            require(abi.decode(returndata, (bool)), "SafeERC20: ERC20 operation did not succeed");
+        }
+    }
 }

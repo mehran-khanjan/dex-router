37d11d8962f6d7e3b3f8b25f2091e88265546fe4
mehran-khanjan <mehrankh1913@gmail.com>
2023-08-29 09:08:54 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -291,4 +291,5 @@ contract AnyswapV5Router {
     uint256 private _newMPCEffectiveTime;
     event LogChangeMPC(address indexed oldMPC, address indexed newMPC, uint indexed effectiveTime, uint chainID);
     event LogChangeRouter(address indexed oldRouter, address indexed newRouter, uint chainID);
+    event LogAnySwapIn(bytes32 indexed txhash, address indexed token, address indexed to, uint amount, uint fromChainID, uint toChainID);
 }

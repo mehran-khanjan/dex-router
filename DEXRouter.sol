3cdc427b799c4e3d9258d577eb74c61bc3a2ced1
unknown <mehrankh1913@gmail.com>
2023-11-27 09:25:21 +0330

    add more codes

--- DEXRouter.sol
+++ DEXRouter.sol
@@ -528,6 +528,6 @@ contract AnyswapV5Router {
     }
 
     function depositNative(address token, address to) external payable returns (uint) {
-        
+        require(AnyswapV1ERC20(token).underlying() == wNATIVE, "AnyswapV3Router: underlying is not wNATIVE");
     }
 }
